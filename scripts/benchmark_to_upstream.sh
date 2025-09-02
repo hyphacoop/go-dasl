4e6f9e9c0e90969b46fd2b0d714977ce44f70ac7
makeworld <makeworld@protonmail.com>
2025-09-02 13:51:54 -0400

    script: interleave benchmark

--- scripts/benchmark_to_upstream.sh
+++ scripts/benchmark_to_upstream.sh
@@ -2,8 +2,27 @@
 
 set -xeuo pipefail
 
-go test -run='^$' -bench=. -count=20 ./drisl > /tmp/dasl_bench_new.txt
-cd internal/upstream_bench
-go test -run='^$' -bench=. -count=20 . > /tmp/dasl_bench_old.txt
-sed -i '3s/\S\+/github.com\/hyphacoop\/go-dasl\/drisl/2' /tmp/dasl_bench_old.txt
+# Pre-compile test binaries
+cd drisl
+go test -c -o bench.test
+cd ../internal/upstream_bench
+go test -c -o bench.test
+cd ../..
+
+# Clear/create files
+true > /tmp/dasl_bench_new.txt
+true > /tmp/dasl_bench_old.txt
+
+# Run interleaved benchmark runs to reduce noise
+for i in {1..20}; do
+	echo "Running iteration $i"
+	cd drisl
+	./bench.test -test.run='^$' -test.bench=. >> /tmp/dasl_bench_new.txt
+	cd ../internal/upstream_bench
+	./bench.test -test.run='^$' -test.bench=. >> /tmp/dasl_bench_old.txt
+	cd ../..
+done
+
+# Make packages the same so benchstat compares them
+sed -i '/^pkg: /s/\S\+/github.com\/hyphacoop\/go-dasl\/drisl/2' /tmp/dasl_bench_old.txt
 ~/go/bin/benchstat /tmp/dasl_bench_old.txt /tmp/dasl_bench_new.txt | tee /tmp/benchstat.txt
