e400b486dabd6c8f119c7b5b13873753b900d91c
makeworld <makeworld@protonmail.com>
2025-09-02 15:37:04 -0400

    benchmark: large CID

--- internal/upstream_bench/bench_large_test.go
+++ internal/upstream_bench/bench_large_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"os"
 	"testing"
+
+	"github.com/hyphacoop/go-dasl/cid"
 )
 
 func BenchmarkMarshalTwitter(b *testing.B) {
@@ -35,6 +37,61 @@ func BenchmarkUnmarshalTwitter(b *testing.B) {
 	var v any
 	if err := json.Unmarshal(data, &v); err != nil {
 		panic(err)
+	}
+	// Marshal and measure
+	em := encMode()
+	marshaled, err := em.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	b.SetBytes(int64(len(marshaled)))
+	// Benchmark
+	dm := decMode()
+	for b.Loop() {
+		var v any
+		dm.Unmarshal(marshaled, &v)
+	}
+}
+
+func BenchmarkMarshalTwitterCid(b *testing.B) {
+	data, err := os.ReadFile("testdata/twitter.json")
+	if err != nil {
+		panic(err)
+	}
+	var v any
+	if err := json.Unmarshal(data, &v); err != nil {
+		panic(err)
+	}
+	// Add in CIDs
+	cid := cid.MustNewCidFromString("bafkreifn5yxi7nkftsn46b6x26grda57ict7md2xuvfbsgkiahe2e7vnq4")
+	for _, status := range v.(map[string]any)["statuses"].([]any) {
+		status.(map[string]any)["cid"] = cid
+	}
+	em := encMode()
+	bytes, err := em.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	b.SetBytes(int64(len(bytes)))
+	for b.Loop() {
+		em.Marshal(v)
+	}
+}
+
+func BenchmarkUnmarshalTwitterCid(b *testing.B) {
+	// Load
+	data, err := os.ReadFile("testdata/twitter.json")
+	if err != nil {
+		panic(err)
+	}
+	var v any
+	if err := json.Unmarshal(data, &v); err != nil {
+		panic(err)
+	}
+	// Add in CIDs
+	cid := cid.MustNewCidFromString("bafkreifn5yxi7nkftsn46b6x26grda57ict7md2xuvfbsgkiahe2e7vnq4")
+	for _, status := range v.(map[string]any)["statuses"].([]any) {
+		status.(map[string]any)["cid"] = cid
 	}
 	// Marshal and measure
 	em := encMode()
